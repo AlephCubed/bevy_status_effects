7afd2382506022e9721e1bde50bbafbc20e59fe1
AlephCubed <jothebro500@gmail.com>
2025-03-24 18:53:02 -0700

    Init.

---
+++ src/lib.rs
@@ -0,0 +1,44 @@
+use bevy_app::{App, Plugin, PreUpdate};
+use bevy_asset::Handle;
+use bevy_ecs::prelude::{Commands, Component, Entity, Query, Res};
+use bevy_image::Image;
+use bevy_time::{Time, Timer};
+
+pub struct StatusEffectPlugin;
+
+impl Plugin for StatusEffectPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(PreUpdate, despawn_finished_lifetimes);
+    }
+}
+
+/// Stores the entity that is being effected by this status effect.
+#[derive(Component)]
+#[relationship(relationship_target = EffectedBy)]
+pub struct Effects(pub Entity);
+
+/// Stores all the status effects that are effecting this entity.
+#[derive(Component)]
+#[relationship_target(relationship = Effects, linked_spawn)]
+pub struct EffectedBy(Vec<Entity>);
+
+/// Despawns the entity when the timer finishes.
+#[derive(Component)]
+pub struct Lifetime(pub Timer);
+
+fn despawn_finished_lifetimes(
+    mut commands: Commands,
+    time: Res<Time>,
+    mut query: Query<(Entity, &mut Lifetime)>,
+) {
+    for (entity, mut lifetime) in &mut query {
+        lifetime.0.tick(time.delta());
+
+        if lifetime.0.finished() {
+            commands.entity(entity).despawn();
+        }
+    }
+}
+
+/// The icon of a status effect.
+pub struct Icon(pub Handle<Image>);
