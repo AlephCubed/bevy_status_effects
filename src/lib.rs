ecca3572107a7e4245261b37fe224e06805046c5
AlephCubed <jothebro500@gmail.com>
2025-04-03 09:51:02 -0700

    Renamed `Effects` to `Effecting`.

--- src/lib.rs
+++ src/lib.rs
@@ -16,11 +16,11 @@ impl Plugin for StatusEffectPlugin {
 /// Stores the entity that is being effected by this status effect.
 #[derive(Component)]
 #[relationship(relationship_target = EffectedBy)]
-pub struct Effects(pub Entity);
+pub struct Effecting(pub Entity);
 
 /// Stores all the status effects that are effecting this entity.
 #[derive(Component)]
-#[relationship_target(relationship = Effects, linked_spawn)]
+#[relationship_target(relationship = Effecting, linked_spawn)]
 pub struct EffectedBy(Vec<Entity>);
 
 impl<'a> IntoIterator for &'a EffectedBy {
