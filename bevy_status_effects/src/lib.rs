d76adade0dfadd989376a9685267da37d167d49c
AlephCubed <jothebro500@gmail.com>
2025-04-03 13:10:33 -0700

    Started work on hooks, trait, and macros.

---
+++ bevy_status_effects/src/lib.rs
@@ -0,0 +1,91 @@
+pub mod relation;
+pub mod timer;
+
+use crate::relation::{EffectedBy, Effecting};
+use bevy_app::{App, Plugin, PreUpdate};
+use bevy_asset::Handle;
+use bevy_ecs::component::HookContext;
+use bevy_ecs::prelude::*;
+use bevy_ecs::world::DeferredWorld;
+use bevy_image::Image;
+
+pub struct StatusEffectPlugin;
+
+impl Plugin for StatusEffectPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(
+            PreUpdate,
+            (timer::despawn_finished_lifetimes, timer::tick_delay).chain(),
+        );
+    }
+}
+
+/// The icon of a status effect.
+pub struct Icon(pub Handle<Image>);
+
+pub trait StatusEffect {
+    const TYPE: EffectType = EffectType::Stack;
+}
+
+/// Describes the logic used when multiple of the same effect are applied to the same entity.
+#[derive(Eq, PartialEq, Default, Debug)]
+pub enum EffectType {
+    #[default]
+    Stack,
+    Refresh,
+}
+
+pub fn init_effect_hook<T: Component + StatusEffect>(world: &mut World) {
+    if T::TYPE == EffectType::Stack {
+        return;
+    }
+
+    world
+        .register_component_hooks::<T>()
+        .on_add(effect_refresh_hook::<T>);
+}
+
+fn effect_refresh_hook<T: Component + StatusEffect>(
+    mut world: DeferredWorld,
+    context: HookContext,
+) {
+    let Some(target) = world.get::<Effecting>(context.entity) else {
+        return;
+    };
+
+    let effects = world
+        .get::<EffectedBy>(target.0)
+        .unwrap()
+        .collection()
+        .clone();
+
+    for effect in effects {
+        if world.get::<T>(effect).is_some() {
+            world.commands().entity(effect).despawn();
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate as bevy_status_effects;
+    use bevy_status_effects_macros::StatusEffect;
+
+    #[derive(StatusEffect)]
+    struct Default;
+
+    #[test]
+    fn default() {
+        assert_eq!(Default::TYPE, EffectType::Stack);
+    }
+
+    #[derive(StatusEffect)]
+    #[effect_type(Refresh)]
+    struct Overriden;
+
+    #[test]
+    fn overriden() {
+        assert_eq!(Overriden::TYPE, EffectType::Refresh);
+    }
+}
---
+++ bevy_status_effects_macros/src/lib.rs
@@ -0,0 +1,90 @@
+use proc_macro_error::{emit_error, proc_macro_error};
+use proc_macro2::{Ident, TokenStream};
+use quote::quote;
+use syn::{Attribute, DeriveInput, Token};
+
+#[proc_macro_derive(StatusEffect, attributes(add_component, effect_type))]
+#[proc_macro_error]
+pub fn stat_container_derive(item: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let tree: DeriveInput = syn::parse(item).expect("TokenStream must be valid.");
+
+    let mut systems = Vec::new();
+    let mut trait_impl = None;
+
+    let struct_name = &tree.ident;
+
+    for attr in &tree.attrs {
+        let path = attr.meta.path();
+
+        let Some(ident) = path.get_ident() else {
+            continue;
+        };
+
+        match ident.to_string().as_str() {
+            "add_component" => parse_add_component(attr, struct_name, &mut systems),
+            "effect_type" => trait_impl = parse_effect_type(attr, struct_name),
+            _ => continue,
+        };
+    }
+
+    let trait_impl = trait_impl.unwrap_or_else(|| {
+        quote! {
+            impl bevy_status_effects::StatusEffect for #struct_name {}
+        }
+    });
+
+    quote! {
+        #trait_impl
+        #(#systems)*
+    }
+    .into()
+}
+
+fn parse_add_component(attr: &Attribute, struct_name: &Ident, systems: &mut Vec<TokenStream>) {
+    attr.parse_nested_meta(|meta| {
+        let Some(var_name) = meta.path.segments.first() else {
+            return Ok(())
+        };
+
+        if var_name.ident.to_string() != "plugin" {
+            return Ok(())
+        };
+
+        let input = &meta.input;
+
+        input.parse::<Token![=]>().expect("An equals sign.");
+        let input = input.parse::<Ident>().expect("An identifier.");
+
+        systems.push(quote! {
+            #[bevy_butler::add_system(generics = <#struct_name>, plugin = #input, schedule = immediate_stats::Startup)]
+            use bevy_status_effects::init_effect_hook;
+        });
+
+        Ok(())
+    }).unwrap();
+}
+
+fn parse_effect_type(attr: &Attribute, struct_name: &Ident) -> Option<TokenStream> {
+    let mut output = None;
+    attr.parse_nested_meta(|meta| {
+        let Some(var_name) = meta.path.segments.first() else {
+            return Ok(())
+        };
+
+        let ident = &var_name.ident;
+
+        if output.is_some() {
+            emit_error!(ident.span(), "Already defined.");
+        }
+
+        output = Some(quote! {
+            impl bevy_status_effects::StatusEffect for #struct_name {
+                const TYPE: bevy_status_effects::EffectType = bevy_status_effects::EffectType::#ident;
+            }
+        });
+
+        Ok(())
+    }).unwrap();
+
+    output
+}
